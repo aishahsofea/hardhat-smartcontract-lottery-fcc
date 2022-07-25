09a12e4fb268da714b50d6267bb4b20a1669d484
Aishah Sofea <aishah.s.hanifa@gmail.com>
2022-07-25 22:48:23 +0800

    create Raffle contract

---
+++ contracts/Raffle.sol
@@ -0,0 +1,45 @@
+// Raffle contract
+
+// Enter the lottery (paying some amount)
+// Pick a random winner (verifiably random)
+// Winner to be selected every X minutes --> completely automated
+
+// Chainlink Oracle --> Randomness, Automated Execution (Chainlink Keeper)
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+error Raffle_NotEnoughETHEntered();
+
+contract Raffle {
+    /* State Variables */
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /* Events */
+    event RaffleEnter(address indexed player);
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle_NotEnoughETHEntered();
+        }
+        s_players.push(payable(msg.sender));
+        // Emit an event when we update a dynamic array or mapping
+        emit RaffleEnter(msg.sender);
+    }
+
+    // function pickRandomWinner() {}
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
