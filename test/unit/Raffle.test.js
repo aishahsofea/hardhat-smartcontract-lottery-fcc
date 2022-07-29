eb3822334b5238bc0b82761d327bea46dc6ba601
Aishah Sofea <aishah.s.hanifa@gmail.com>
2022-07-30 00:33:10 +0800

    unit tests continued

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -5,7 +5,7 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
 !developmentChains.includes(network.name)
   ? describe.skip
   : describe("Raffle", function () {
-      let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, deployer;
+      let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, deployer, interval;
       const chainId = network.config.chainId;
 
       beforeEach(async () => {
@@ -14,6 +14,7 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
         raffle = await ethers.getContract("Raffle", deployer);
         vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer);
         raffleEntranceFee = await raffle.getEntranceFee();
+        interval = await raffle.getInterval();
       });
 
       describe("constructor", function () {
@@ -35,6 +36,24 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
           await raffle.enterRaffle({ value: raffleEntranceFee });
           const playerFromContract = await raffle.getPlayer(0);
           assert.equal(playerFromContract, deployer);
+        });
+
+        it("emits event on enter", async function () {
+          await expect(raffle.enterRaffle({ value: raffleEntranceFee })).to.emit(
+            raffle,
+            "RaffleEnter"
+          );
+        });
+
+        it("does not allow entrance when raffle is calculating", async function () {
+          await raffle.enterRaffle({ value: raffleEntranceFee });
+          await network.provider.send("evm_increaseTime", [interval.toNumber() + 1]);
+          await network.provider.send("evm_mine", []);
+          // pretend to be a Chainlink Keeper
+          await raffle.performUpkeep([]);
+          await expect(raffle.enterRaffle({ value: raffleEntranceFee })).to.be.revertedWith(
+            "Raffle__NotOpen"
+          );
         });
       });
     });
